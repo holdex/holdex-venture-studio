864480224518a7cad7264ed5ecd409a293b70736
George Ciubotaru <geeku96@gmail.com>
2023-04-18 10:15:39 +0300

    feat: hook client errors

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -2,14 +2,10 @@ import rollbar from '$components/Rollbar';
 
 /** @type {import('@sveltejs/kit').HandleClientError} */
 export const handleError = async ({ error, event }) => {
-    const headers = {};
-    event.request.headers.forEach((v, k) => (headers[k] = v));
-
     rollbar.error({
         message: error.message || "Client error",
         stack: error?.networkError || error?.graphQLErrors || error
     }, {
-        headers: headers,
         url: event.url,
         method: event.request.method
     });
