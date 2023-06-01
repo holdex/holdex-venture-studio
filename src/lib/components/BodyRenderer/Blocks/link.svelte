13f179d9f97955baa5e770c378f0616d286a5bdf
George Ciubotaru <geeku96@gmail.com>
2023-06-01 13:00:24 +0300

    feat: apply changes

--- src/lib/components/BodyRenderer/Blocks/link.svelte
+++ src/lib/components/BodyRenderer/Blocks/link.svelte
@@ -23,17 +23,23 @@
 			classes += 'text-paragraph-l';
 			break;
 	}
-	let truncateUrl = (url: string) => {
 
-		let textWithoutPrefix = url.replace(/^https?:\/\//, '');
-		let domain = textWithoutPrefix.split('/')[0];
-		let path = textWithoutPrefix.slice(domain.length + 1).length ? textWithoutPrefix.slice(domain.length + 1) : '';
-		let truncatedUrl = path.length > 8 ? domain + '/' + textWithoutPrefix.slice(domain.length + 1, domain.length + 1 + 8) + '...' : textWithoutPrefix;
-		return truncatedUrl;
-	}
 	$: text = item.text || item.href;
 	$: truncated = text.includes('http') ? truncateUrl(text) : text;
 	$: isHoldexLink = regExp.holdexLink.test(item.href);
+
+	let truncateUrl = (url: string) => {
+		let textWithoutPrefix = url.replace(/^https?:\/\//, '');
+		let domain = textWithoutPrefix.split('/')[0];
+		let path = textWithoutPrefix.slice(domain.length + 1).length
+			? textWithoutPrefix.slice(domain.length + 1)
+			: '';
+		let truncatedUrl =
+			path.length > 8
+				? domain + '/' + textWithoutPrefix.slice(domain.length + 1, domain.length + 1 + 8) + '...'
+				: textWithoutPrefix;
+		return truncatedUrl;
+	};
 </script>
 
 {' '}
@@ -43,9 +49,8 @@
 	class={classes}
 	target={isHoldexLink ? '_self' : '_blank'}
 	rel="noreferrer"
-
 >
-	<slot {truncated} />
+	<slot text={truncated} />
 	{#if !isHoldexLink}
 		<Icon icon={ArrowTopRightOnSquare} width={16} height={16} colorInherit />
 	{/if}
