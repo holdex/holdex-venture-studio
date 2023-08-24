032d7476a93f91ca359ab9abeaf09ea398731821
surajsharma <surajsharma@pm.me>
2023-08-24 13:01:34 +0530

    feat: table responsive scrolling with indicators

--- src/lib/components/BodyRenderer/Blocks/table.svelte
+++ src/lib/components/BodyRenderer/Blocks/table.svelte
@@ -1,10 +1,40 @@
 <script lang="ts">
   /* eslint-disable @typescript-eslint/no-unused-vars */
-  import Item from '../item.svelte'
+  import Item from '../item.svelte';
 
   export let cells: any[];
+
+  let secondaryNavScrollLeft = 0;
+  let isLeftEnd = true;
+  let isRightEnd = false;
+  let w;
+
+  const scrollAction = (node: HTMLElement) => {
+    const hasReachedRightEnd = () => {
+      const navbarSectionElement = document.getElementById('table-scroll');
+
+      if (!node || !navbarSectionElement) {
+        return;
+      }
+
+      secondaryNavScrollLeft = node?.scrollLeft;
+      isLeftEnd = secondaryNavScrollLeft === 0;
+      isRightEnd =
+        secondaryNavScrollLeft + node.clientWidth === navbarSectionElement.clientWidth
+          ? false
+          : true;
+    };
+
+    node.addEventListener('scroll', hasReachedRightEnd, false);
+    return {
+      destory() {
+        node.removeEventListener('scroll', hasReachedRightEnd);
+      },
+    };
+  };
 </script>
 
-<template lang="pug" src="./table.pug">
-
-</template>
+<div class="w-full" bind:clientWidth={w}>
+  <template lang="pug" src="./table.pug">
+  </template>
+</div>
