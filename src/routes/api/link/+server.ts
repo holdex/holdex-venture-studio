bae58b138be793e037ca764124e460d14f6ba722
surajsharma <surajsharma@pm.me>
2023-09-04 11:43:36 +0530

    feat: og api route

--- src/routes/api/link/+server.ts
+++ src/routes/api/link/+server.ts
@@ -1,55 +1,31 @@
-import { default as clientConfig } from '$lib/config';
-import config from '$lib/server/config';
 import { json, type RequestHandler } from '@sveltejs/kit';
 import ogs from 'open-graph-scraper';
-export const GET: RequestHandler = async ({ fetch, url, params }) => {
-  const urlin = url.searchParams.get('url');
+
+export const GET: RequestHandler = async ({ url }) => {
+  const urlin = url.searchParams.get('url') || undefined;
   const options = { url: urlin };
 
-  ogs(options).then((data) => {
-    const { error, html, result, response } = data;
-    console.log('error:', error); // This returns true or false. True if there was an error. The error itself is inside the result object.
-    console.log('html:', html); // This contains the HTML of page
-    console.log('result:', result); // This contains all of the Open Graph results
-    console.log('response:', response); // This contains response from the Fetch API
-  });
-  //   const og =
+  if (!urlin) return json({ message: 'Invalid URL', status: 400 });
 
-  return json(
-    {
-      result: urlin,
-    },
-    {
-      status: 200,
+  try {
+    const data = await ogs(options);
+
+    if (data) {
+      const { error, result } = data;
+      const { ogDescription, ogTitle, ogImage } = result;
+      if (result && !error) {
+        return json(
+          {
+            result: { ogDescription, ogTitle, ogImage },
+          },
+          {
+            status: 200,
+          }
+        );
+      }
     }
-  );
-
-  //   const ticker = url.searchParams.get('ticker');
-
-  //   if (!ticker) {
-  //     return json({ error: 'invalid_arguments' }, { status: 400 });
-  //   }
-
-  //   return fetch(`${clientConfig.utilsApiUrl}/exchange-rate?ticker=${ticker}`, {
-  //     headers: {
-  //       'x-holdex-authorization': `Bearer ${config.utilsApiKey}`,
-  //     },
-  //   })
-  //     .then((res) => res.json())
-  //     .then((tickerInfo) => {
-  //       if (!tickerInfo.error) {
-  //         return json(
-  //           {
-  //             change: tickerInfo.data.changePercent24Hr || 0,
-  //             price: tickerInfo.data.priceUsd || 0,
-  //           },
-  //           { status: 200 }
-  //         );
-  //       }
-
-  //       return json({ error: tickerInfo.error }, { status: 400 });
-  //     })
-  //     .catch((err) => {
-  //       return json({ error: err }, { status: 400 });
-  //     });
+  } catch (error) {
+    console.log('Call to opengraph failed.', error!.result?.error);
+    return json({ status: 400, error: error!.result?.error });
+  }
 };
