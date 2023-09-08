d3d4baec7b3f9a673bf83bb04c8f5e77ac0503c4
surajsharma <surajsharma@pm.me>
2023-09-08 12:10:51 +0530

    fix: cleanup

--- src/lib/components/BodyRenderer/Blocks/table.svelte
+++ src/lib/components/BodyRenderer/Blocks/table.svelte
@@ -6,7 +6,6 @@
   export let cells: any[];
 
   let totalCols = cells[0].length;
-  console.log('🚀 ~ file: table.svelte:10 ~ totalCols:', totalCols);
 
   let isLeftEnd = true;
   let isRightEnd = false;
