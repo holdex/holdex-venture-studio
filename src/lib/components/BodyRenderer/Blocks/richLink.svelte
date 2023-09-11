e28150129d4afac6895ff9d777f34e75304ee775
surajsharma <surajsharma@pm.me>
2023-09-12 00:35:01 +0530

    fix: review

--- src/lib/components/BodyRenderer/Blocks/richLink.svelte
+++ src/lib/components/BodyRenderer/Blocks/richLink.svelte
@@ -4,6 +4,7 @@
   import Icon from '$components/Icons/index.svelte';
   import { isBrowser } from '$lib/config';
   import { Link } from '@steeze-ui/heroicons';
+  import { getContext } from 'svelte';
 
   type Item = {
     type: string;
@@ -15,7 +16,7 @@
   export let item: Item;
 
   let classes =
-    'inline-block text-footnote block underline underline-offset-4 bg-accent1-default/15 text-accent1-default transition-colors hover:bg-accent1-default/25 focus:bg-accent1-default/25';
+    'cursor-pointer inline-block text-footnote block underline underline-offset-4 bg-accent1-default/15 text-accent1-default transition-colors hover:bg-accent1-default/25 focus:bg-accent1-default/25';
 
   let truncateUrl = (url: string) => {
     let textWithoutPrefix = url.replace(/^https?:\/\//, '');
@@ -31,27 +32,59 @@
     return truncatedUrl;
   };
 
+  let loading = false;
   let loadOgData = async (url: string) => {
     if (isBrowser) {
-      return fetch(`/api/link?url=${url}`).then((res) => res.json());
+      loading = true;
+      const ogData = await fetch(`/api/link?url=${url}`).then((res) => res.json());
+      loading = false;
+      return ogData;
     }
   };
 
   $: truncated = truncateUrl(item?.data?.url);
   $: isHoldexLink = regExp.holdexLink.test(item?.data?.url);
+
+  let promise = loadOgData(item?.data?.url);
 </script>
 
-{#await loadOgData(item?.data?.url) then ogdata}
-  <div class="flex border border-l4 bg-l2 rounded-xl w-full items-center h-24">
+<a
+  href={item?.data?.url}
+  class="flex border border-l4 bg-l2 rounded-xl w-full items-center h-24"
+  target={isHoldexLink ? '_self' : '_blank'}
+>
+  {#await promise}
+    <div class="h-full flex items-center">
+      <div
+        class="w-full rich-link-image-skeleton h-full rounded-xl bg-contain bg-no-repeat bg-center animation"
+      />
+      <div class="p-4 flex flex-col text-footnote justify-between gap-1 w-full overflow-hidden">
+        <div class="animation animate h-4 skeleton-desc" />
+        <div class="animation animate h-4" />
+        <div>
+          <a
+            title={' '}
+            href={item.data?.url}
+            target={isHoldexLink ? '_self' : '_blank'}
+            class={classes}
+            rel="noreferrer"
+          >
+            {truncated}
+            {#if !isHoldexLink}
+              <Icon icon={ArrowTopRightOnSquare} width={16} height={16} colorInherit />
+            {/if}
+          </a>
+        </div>
+      </div>
+    </div>
+  {:then ogdata}
     <div class="h-full flex items-center">
       {#if ogdata.image}
         <div
-          class="rich-link-image h-full rounded-xl bg-contain"
-          style={`object-fit: cover; overflow:hidden;
-          `}
-        >
-          <img class="h-full rounded-xl" src={ogdata?.image[0]?.url} alt={ogdata.title} />
-        </div>
+          class="rich-link-image h-full rounded-xl bg-contain bg-no-repeat bg-center"
+          style={`background-size:cover; background-position: center;
+            background-repeat: no-repeat; background-image: url(${ogdata?.image[0]?.url});`}
+        />
       {:else}
         <div
           class="w-[40px] h-[40px] bg-l3 ml-4 rounded-full flex items-center justify-center p-2 text-t3"
@@ -66,10 +99,10 @@
       <div class="text-t3 text-ellipsis truncate">{ogdata.desc}</div>
       <div>
         <a
-          title={item.title ? item.title : ''}
-          href={item.href}
-          class={classes}
+          title={item?.data?.title || ''}
+          href={item?.data?.url}
           target={isHoldexLink ? '_self' : '_blank'}
+          class={classes}
           rel="noreferrer"
         >
           {truncated}
@@ -80,5 +113,27 @@
         </a>
       </div>
     </div>
-  </div>
-{/await}
+  {/await}
+</a>
+
+<style lang="sass">
+  
+  .skeleton-desc
+    @apply w-[200px]
+
+  .animation
+    @apply bg-gradient-to-r from-l4 to-transparent bg-[400%_100%]
+
+  .animate
+    background-size: 400%
+    -webkit-animation: loading 8s ease infinite
+    -moz-animation: loading 8s ease infinite
+    animation: loading 8s ease infinite
+
+
+  @keyframes loading
+    0%
+      background-position: 200% 0
+    100%
+      background-position: -200% 0
+</style>
