b5e1bb0edbb64957c69d91234de348572e042277
George Ciubotaru <george@holdex.io>
2023-12-22 10:26:11 +0200

    feat: remove hardcoded value

--- src/lib/components/BodyRenderer/index.svelte
+++ src/lib/components/BodyRenderer/index.svelte
@@ -3,24 +3,6 @@
   import Item from './item.svelte';
 
   export let blocks: any[];
-  $: addtionalBlocks = [
-    ...blocks,
-    {
-      type: 'cta',
-      data: {
-        title: '123',
-        description: 'CTa test description',
-        link1: {
-          text: 'Button 1',
-          url: 'https://google.com',
-        },
-        link2: {
-          text: 'Button 2',
-          url: 'https://google.com',
-        },
-      },
-    },
-  ];
 </script>
 
 {#each blocks as item, index}
