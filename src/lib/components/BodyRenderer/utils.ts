966cf00565c6dbbf1793babe11d68ed7c9141589
ogin <danvalleybarrick@gmail.com>
2024-01-16 11:19:01 -0800

    chore: clean code

--- src/lib/components/BodyRenderer/utils.ts
+++ src/lib/components/BodyRenderer/utils.ts
@@ -11,21 +11,13 @@ export function parseTestimonialSection(content: any[]) {
       contentHead[1][0].items[0].text === 'testimonial'
     ) {
       const data: any = {};
-      console.log('ogin: parseTestimonialSection:', content);
-      console.log('ogin: parseTestimonialSection: if', content);
-      console.log('ogin: data', contentHead[0][0]);
-      console.log('ogin: data', contentHead[1][0]);
-      console.log('ogin: parseTestimonialSection: data', data);
       content.forEach(([[first], [second]], i) => {
-        console.log('ogin: parseTestimonialSection: first', first);
-        console.log('ogin: parseTestimonialSection: second', second);
         if (first === undefined || first.type !== 'paragraph') return;
         if (second === undefined || second.type !== 'paragraph') data[first.data.text] = '';
         else if (first.items[0].text == 'picture') {
           data[first.items[0].text] = second.items[0].href;
         } else data[first.items[0].text] = second.items[0].text;
       });
-      console.log('ogin: parseTestimonialSection: data', data);
       testimonial.name = data['name'];
       testimonial.title = data['title'];
       testimonial.content = data['content'];
@@ -33,7 +25,6 @@ export function parseTestimonialSection(content: any[]) {
         text: data['picture'],
         url: data['picture'],
       };
-      console.log('ogin: parseTestimonialSection: testimonial', testimonial);
     }
   }
   return testimonial;
