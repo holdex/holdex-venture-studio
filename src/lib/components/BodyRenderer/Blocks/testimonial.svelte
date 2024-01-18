37622d42df7849305cbb1ae1c2ae7150549cc92b
ogin <danvalleybarrick@gmail.com>
2024-01-18 07:24:07 -0800

    feat: detect theme conversion and change border-color by theme

--- src/lib/components/BodyRenderer/Blocks/testimonial.svelte
+++ src/lib/components/BodyRenderer/Blocks/testimonial.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import type { TestimonialElement } from '$components/BodyParser/blocks';
-  import Image from '$components/BodyRenderer/Blocks/image.svelte';
-  import { ArrowTopRightOnSquare } from '$components/Icons';
+  import { getContext } from 'svelte';
   /* eslint-disable @typescript-eslint/no-unused-vars */
 
   export let item: TestimonialElement;
+
+  $: themeContext = getContext('theme');
 </script>
 
 <div
-  class="testimonial relative flex-col justify-start items-stretch w-full border border-solid border-l4 rounded-xl bg-l1 dark:bg-l2"
+  class={'relative flex-col justify-start items-stretch w-full border border-solid border-l4 rounded-xl bg-l1 dark:bg-l2 border-decoration-' +
+    $themeContext}
 >
   <div class="flex flex-row border-b border-solid border-l4 p-4">
     <div class="flex items-center">
@@ -34,11 +36,11 @@
 </div>
 
 <style lang="sass">
-.testimonial
+.border-decoration-dark
   &:before
     content: ""
     position: absolute
-    top: 0px
+    top: -1px
     left: 10%
     width: 200px
     height: 1px
@@ -49,11 +51,34 @@
   &:after
     content: ""
     position: absolute
-    bottom: 0px
+    bottom: -1px
     right: 10%
     width: 200px
     height: 1px
-    background: linear-gradient(90deg, rgba(47, 50, 61, 0.00) 0.5%, rgba(0, 204, 255, 0.25) 25.56%, #0CF 50.63%, rgba(0, 204, 255, 0.25) 75.69%, rgba(47, 50, 61, 0.00) 100.75%)      
+    background: linear-gradient(90deg, rgba(47, 50, 61, 0.00) 0.5%, rgba(0, 204, 255, 0.25) 25.56%, #0CF 50.63%, rgba(0, 204, 255, 0.25) 75.69%, rgba(47, 50, 61, 0.00) 100.75%) 
+    background-repeat: no-repeat
+    @media screen and (max-width: 450px)
+      right: 5%
+.border-decoration-light
+  &:before
+    content: ""
+    position: absolute
+    top: -1px
+    left: 10%
+    width: 200px
+    height: 1px
+    background: linear-gradient(90deg, rgba(47, 50, 61, 0) 0.5%, rgba(46, 131, 229, 0.25) 25.56%, #2E83E5 50.63%, rgba(46, 131, 229, 0.25) 75.69%, rgba(47, 50, 61, 0) 100.75%)      
+    background-repeat: no-repeat
+    @media screen and (max-width: 450px)
+      left: 5%
+  &:after
+    content: ""
+    position: absolute
+    bottom: -1px
+    right: 10%
+    width: 200px
+    height: 1px
+    background: linear-gradient(90deg, rgba(47, 50, 61, 0) 0.5%, rgba(46, 131, 229, 0.25) 25.56%, #2E83E5 50.63%, rgba(46, 131, 229, 0.25) 75.69%, rgba(47, 50, 61, 0) 100.75%)      
     background-repeat: no-repeat
     @media screen and (max-width: 450px)
       right: 5%
