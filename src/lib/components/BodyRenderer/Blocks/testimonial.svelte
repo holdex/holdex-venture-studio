6ce10fee5be1846620f32ee3a5d9a56f3cbbce9c
ogin <danvalleybarrick@gmail.com>
2024-01-18 11:09:20 -0800

    feat(style): optimized style

--- src/lib/components/BodyRenderer/Blocks/testimonial.svelte
+++ src/lib/components/BodyRenderer/Blocks/testimonial.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div
-  class={'relative flex-col justify-start items-stretch w-full border border-solid border-l4 rounded-xl bg-l1 dark:bg-l2 border-decoration-' +
+  class={'border-decoration relative flex-col justify-start items-stretch w-full border border-solid border-l4 rounded-xl bg-l1 dark:bg-l2 border-decoration-' +
     $themeContext}
 >
   <div class="flex flex-row border-b border-solid border-l4 p-4">
@@ -36,52 +36,37 @@
 </div>
 
 <style lang="sass">
+.border-decoration
+  &:before
+    content: ""
+    position: absolute
+    top: -1px
+    left: 10%
+    width: 200px
+    height: 1px
+    background-repeat: no-repeat
+    @media screen and (max-width: 450px)
+      left: 5%
+  &:after
+    content: ""
+    position: absolute
+    bottom: -1px
+    right: 10%
+    width: 200px
+    height: 1px
+    background-repeat: no-repeat
+    @media screen and (max-width: 450px)
+      right: 5%
 .border-decoration-dark
   &:before
-    content: ""
-    position: absolute
-    top: -1px
-    left: 10%
-    width: 200px
-    height: 1px
     background: linear-gradient(90deg, rgba(47, 50, 61, 0.00) 0.5%, rgba(0, 204, 255, 0.25) 25.56%, #0CF 50.63%, rgba(0, 204, 255, 0.25) 75.69%, rgba(47, 50, 61, 0.00) 100.75%) 
-    background-repeat: no-repeat
-    @media screen and (max-width: 450px)
-      left: 5%
   &:after
-    content: ""
-    position: absolute
-    bottom: -1px
-    right: 10%
-    width: 200px
-    height: 1px
     background: linear-gradient(90deg, rgba(47, 50, 61, 0.00) 0.5%, rgba(0, 204, 255, 0.25) 25.56%, #0CF 50.63%, rgba(0, 204, 255, 0.25) 75.69%, rgba(47, 50, 61, 0.00) 100.75%) 
-    background-repeat: no-repeat
-    @media screen and (max-width: 450px)
-      right: 5%
 .border-decoration-light
   &:before
-    content: ""
-    position: absolute
-    top: -1px
-    left: 10%
-    width: 200px
-    height: 1px
     background: linear-gradient(90deg, rgba(47, 50, 61, 0) 0.5%, rgba(46, 131, 229, 0.25) 25.56%, #2E83E5 50.63%, rgba(46, 131, 229, 0.25) 75.69%, rgba(47, 50, 61, 0) 100.75%)      
-    background-repeat: no-repeat
-    @media screen and (max-width: 450px)
-      left: 5%
   &:after
-    content: ""
-    position: absolute
-    bottom: -1px
-    right: 10%
-    width: 200px
-    height: 1px
     background: linear-gradient(90deg, rgba(47, 50, 61, 0) 0.5%, rgba(46, 131, 229, 0.25) 25.56%, #2E83E5 50.63%, rgba(46, 131, 229, 0.25) 75.69%, rgba(47, 50, 61, 0) 100.75%)      
-    background-repeat: no-repeat
-    @media screen and (max-width: 450px)
-      right: 5%
 .avatar 
     background-size: 48px
     width: 48px
