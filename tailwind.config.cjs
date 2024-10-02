300a20a3554f6e8796d5a5e56f88810591186c16
Matthew Vogas <151702554+matthewvogas@users.noreply.github.com>
2024-10-02 16:01:30 -0600

    feat(ui): update landing page hero section (#626)
    * feat(ui): hero page - landing page update
    
    * fix(hero): resolve logo loop, metric designs, title layout issues
    
    * fix(ui): update style hero section
    
    * fix(ui): update deg border kpi box
    
    * fix(ui) typo Concurrent users
    
    * Update src/routes/(pages)/template.pug
    
    Signed-off-by: Vadim <vadim@holdex.io>
    
    * fix: DRY, Comments and styles
    
    * fix: dont repeat same name
    
    * chore: sanitize function to validate styles
    
    ---------
    
    Signed-off-by: Vadim <vadim@holdex.io>
    Co-authored-by: mvttheos <mvttheos@gmail.com>
    Co-authored-by: Mark Curchin <46079535+markholdex@users.noreply.github.com>
    Co-authored-by: Vadim <goldwing@yandex.ru>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -365,6 +365,12 @@ module.exports = {
         }
       },
       backgroundImage: {
+
+        //- Landing Page 
+          //- Hero / KPIs    
+        'subcontainer-gradient': 'linear-gradient(to bottom, #1A1E29, #10141F)', 
+        'subcontainer-gradient-light': 'linear-gradient(to bottom, #EAEBEF, #F0F1F5)', 
+
         'card-1-dark': "url('/cardBg/card-1-bg-dark.png')",
         'card-2-dark': "url('/cardBg/card-2-bg-dark.png')",
         'card-3-dark': "url('/cardBg/card-3-bg-dark.png')",
