ab455fe0e2c4c012178f04906a829b56edcd0d67
Luis Montero <lmonterom879@gmail.com>
2025-01-14 09:46:23 -0600

    fix: fixing typo

--- src/lib/components/TeamMemberCard/index.svelte
+++ src/lib/components/TeamMemberCard/index.svelte
@@ -1,12 +1,16 @@
-<script>
+<script lang="ts">
     import TextParagraph from '$components/TextParagraph/index.svelte';
   
-    export let name;
-    export let role;
-    export let description;
-    export let additionalDescription;
-    export let avatar;
-    export let link;
+    export let name: string;
+    export let role: string;
+    export let description: string;
+    export let additionalDescription: string;
+    export let avatar: string;
+    export let link: string;
+
+    $: if (!name || !role || !description || !additionalDescription || !avatar || !link) {
+        throw new Error('Team member card is missing required props');
+    }
   </script>
   
   <template lang="pug" src="./template.pug"></template>
