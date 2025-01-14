24d17b1a60dcc12c21aa20e1d3d15015686f75df
Luis Montero <lmonterom879@gmail.com>
2025-01-14 09:33:09 -0600

    feat(ui): member card

---
+++ src/lib/components/TeamMemberCard/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    import TextParagraph from '$components/TextParagraph/index.svelte';
+  
+    export let name;
+    export let role;
+    export let description;
+    export let additionalDescription;
+    export let avatar;
+    export let link;
+  </script>
+  
+  <template lang="pug" src="./template.pug"></template>
