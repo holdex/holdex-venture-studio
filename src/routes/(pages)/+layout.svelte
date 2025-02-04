655f52e94ff1b0c23da0d13c6e367fd977642448
Luis Eduardo Montero Molina <106637518+lmontero18@users.noreply.github.com>
2025-02-04 05:08:02 -0600

    feat(api): add page reference to form submissions (#690)
    * pr init
    
    * feat(api): see page url from email
    
    * fix: deleting unnecessary comments
    
    ---------
    
    Co-authored-by: Angelica Willianto <78342026+angelicawill@users.noreply.github.com>

--- src/routes/(pages)/+layout.svelte
+++ src/routes/(pages)/+layout.svelte
@@ -24,7 +24,6 @@
   import Button from '$components/Button/index.svelte';
   import type { SVGIconName } from '$components/Icons/types';
 
-  /** vars */
   let email = '';
   let message = '';
   let name = '';
@@ -37,7 +36,6 @@
   let themeContext = writable(themeIconName === 'sun' ? 'dark' : 'light');
   setContext('theme', themeContext);
 
-  /** funcs */
   const onThemeToggle = () => {
     themeIconName = themeIconName === 'moon' ? 'sun' : 'moon';
     localStorage.setItem('theme', themeIconName === 'moon' ? 'light' : 'dark');
@@ -74,6 +72,9 @@
   const onContactFormSumbit = async (event: Event) => {
     const form = event.currentTarget as HTMLFormElement;
     const data = new FormData(form);
+
+    const currentPage = window.location.pathname;
+    data.append('pageUrl', currentPage);
 
     const response = await fetch(form.action, {
       method: 'POST',
@@ -113,7 +114,6 @@
     return !validateEmail(email) && email.length > 0 ? (isError = true) : (isError = false);
   };
 
-  /** react-ibles */
   $: path = $page.url.pathname;
   $: form = $page.form;
   $: if (globalThis.document) {
