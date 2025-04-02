13d1d3fd49584ea7c5bd6b864478fc90f033ebf3
Screpuh <schaap.marco@gmail.com>
2025-04-02 12:56:18 +0200

    variable link optional and use one param for TeamMemberCard

--- src/lib/components/TeamMemberCard/index.svelte
+++ src/lib/components/TeamMemberCard/index.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-    import TextParagraph from '$components/TextParagraph/index.svelte';
-  
-    export let name: string;
-    export let role: string;
-    export let description: string;
-    export let image: string;
-    export let link: string;
+  import type { TeamMemberBlock } from '$components/BodyParser/blocks';
+  import TextParagraph from '$components/TextParagraph/index.svelte';
 
-    $: if (!name || !role || !description || !image || !link) {
-        throw new Error('Team member card is missing required props');
-    }
-  </script>
-  
-  <template lang="pug" src="./template.pug"></template>
+  export let item: TeamMemberBlock;
+
+  let { name, role, description, image, link } = item.data;
+
+  $: if (!name || !role || !description || !image) {
+    throw new Error('Team member card is missing required props');
+  }
+</script>
+
+<template lang="pug" src="./template.pug"></template>
