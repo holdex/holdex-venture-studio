882ab319f225ea8afa12cc29789511de340c0e79
Marco <schaap.marco@gmail.com>
2025-04-16 22:55:01 +0200

    feat(ui): view new testimonial component in articles (#734)
    * rebase main into brange
    
    * rename endorsement and render new testimonial element
    
    * rename testimonials
    
    * move testimonial back to svelte component renderer
    
    * directly call Link component
    
    * remove x padding on tablet screen
    
    * can remove py as well
    
    * remove padding for large screens
    
    * add padding left on large screen
    
    ---------
    
    Co-authored-by: Vadim <goldwing@yandex.ru>
    Co-authored-by: Angelica Willianto <78342026+angelicawill@users.noreply.github.com>

--- src/lib/components/BodyRenderer/Blocks/testimonial.svelte
+++ src/lib/components/BodyRenderer/Blocks/testimonial.svelte
@@ -1,76 +1,38 @@
 <script lang="ts">
   import type { TestimonialElement } from '$components/BodyParser/blocks';
-  import { getContext } from 'svelte';
-  import type { Writable } from 'svelte/store';
-  /* eslint-disable @typescript-eslint/no-unused-vars */
-
+  import Link from '$components/BodyRenderer/Blocks/link.svelte';
   export let item: TestimonialElement;
 
-  let themeContext: Writable<string> = getContext('theme');
+  const picture =
+    item.picture?.url && item.picture.url.trim() !== ''
+      ? item.picture.url
+      : '/empty-image-thumb.png';
 </script>
 
 <div
-  class={'border-decoration relative flex-col justify-start items-stretch w-full border border-solid border-l4 rounded-xl bg-l1 dark:bg-l2 border-decoration-' +
-    $themeContext}
+  class="testimonial-wrapper flex flex-col gap-4 w-full my-12 items-center smDown:items-start xl-up:py-10 xl-up:pl-21 pt-8 smDown:px-4"
 >
-  <div class="flex flex-row border-b border-solid border-l4 p-4">
-    <div class="flex items-center">
-      {#if item.picture}
-        <img class="w-12 h-12 rounded-full object-cover object-center" src={item.picture.url} />
-      {/if}
+  <div class="testimonial relative">
+    <span
+      class="absolute text-[201px] font-satoshi text-t3 -top-36 -left-25 tb:-left-2 tb:-top-24 tb:text-9xl sm:text-9xl sm:-top-20 sm:left-1 xs:text-9xl xs:-top-18 xs:left-1"
+      >“</span
+    >
+    <div class="testimonial-text">
+      <blockquote class="text-paragraph-l text-t2 xs:text-paragraph-s max-w-[604px]">
+        {@html item.content}
+      </blockquote>
     </div>
-    <div class="flex flex-col xs:w-full ml-4">
-      {#if item.name}
-        <p class="text-paragraph-l text-t1 font-bold">{item.name}</p>
-      {/if}
-
-      {#if item.title}
-        <p class="text-paragraph-l text-t3">{item.title}</p>
-      {/if}
+    <div class="author-info flex items-center justify-end gap-4 mt-4">
+      <div
+        class="author-details flex flex-col items-end text-right font-satoshi text-h5-l smDown:text-h5-s"
+      >
+        <Link
+          item={{ type: 'link', href: item.link, text: item.name, title: item.name, iconSize: 20 }}
+          >{item.name}</Link
+        >
+        <span class="text-h5-l smDown:text-h5-s w-fit">{item.title}</span>
+      </div>
+      <img src={picture} alt={item.name} class="h-14 w-14 rounded-xl" />
     </div>
-  </div>
-  <div class="flex flex-col xs:w-full p-4">
-    {#if item.content}
-      <p class="text-paragraph-l text-t2">{item.content}</p>
-    {/if}
   </div>
 </div>
-
-<style lang="sass">
-.border-decoration
-  &:before
-    content: ""
-    position: absolute
-    top: -1px
-    left: 10%
-    width: 200px
-    height: 1px
-    background-repeat: no-repeat
-    @media screen and (max-width: 450px)
-      left: 5%
-  &:after
-    content: ""
-    position: absolute
-    bottom: -1px
-    right: 10%
-    width: 200px
-    height: 1px
-    background-repeat: no-repeat
-    @media screen and (max-width: 450px)
-      right: 5%
-.border-decoration-dark
-  &:before
-    background: linear-gradient(90deg, rgba(47, 50, 61, 0.00) 0.5%, rgba(0, 204, 255, 0.25) 25.56%, #0CF 50.63%, rgba(0, 204, 255, 0.25) 75.69%, rgba(47, 50, 61, 0.00) 100.75%) 
-  &:after
-    background: linear-gradient(90deg, rgba(47, 50, 61, 0.00) 0.5%, rgba(0, 204, 255, 0.25) 25.56%, #0CF 50.63%, rgba(0, 204, 255, 0.25) 75.69%, rgba(47, 50, 61, 0.00) 100.75%) 
-.border-decoration-light
-  &:before
-    background: linear-gradient(90deg, rgba(47, 50, 61, 0) 0.5%, rgba(46, 131, 229, 0.25) 25.56%, #2E83E5 50.63%, rgba(46, 131, 229, 0.25) 75.69%, rgba(47, 50, 61, 0) 100.75%)      
-  &:after
-    background: linear-gradient(90deg, rgba(47, 50, 61, 0) 0.5%, rgba(46, 131, 229, 0.25) 25.56%, #2E83E5 50.63%, rgba(46, 131, 229, 0.25) 75.69%, rgba(47, 50, 61, 0) 100.75%)      
-.avatar 
-    background-size: 48px
-    width: 48px
-    height: 48px
-
-</style>
