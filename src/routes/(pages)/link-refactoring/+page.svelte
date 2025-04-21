279f957424468e4cdceed877a438274a255be8c0
Addis Belete Bejiga <addis.bel@gmail.com>
2025-04-21 10:43:36 +0300

    chore: format

--- src/routes/(pages)/link-refactoring/+page.svelte
+++ src/routes/(pages)/link-refactoring/+page.svelte
@@ -10,37 +10,37 @@
   };
 
   const link: Item = {
-  type: 'link',
-  text: 'Link 1',
-  href: 'https://holdex.io',
-  title: 'A link component with type "link" and a Holdex URL.',
-};
+    type: 'link',
+    text: 'Link 1',
+    href: 'https://holdex.io',
+    title: 'A link component with type "link" and a Holdex URL.',
+  };
 
-const link2: Item = {
-  type: 'heading-link',
-  text: 'Link 2',
-  href: 'https://clearpool.finance',
-  title: 'A heading link with an external URL.',
-};
+  const link2: Item = {
+    type: 'heading-link',
+    text: 'Link 2',
+    href: 'https://clearpool.finance/lending-prime-',
+    title: 'A heading link with an external URL.',
+  };
 
-const link3: Item = {
-  type: 'article-heading-link',
-  text: 'Link 3',
-  href: '/about',
-  title: 'An article heading link with an internal URL.',
-};
+  const link3: Item = {
+    type: 'article-heading-link',
+    text: 'Link 3',
+    href: '/about',
+    title: 'An article heading link with an internal URL.',
+  };
 
-const link4: Item = {
-  type: 'article-heading-link',
-  text: 'https://clearpool.finance',
-  href: 'https://clearpool.finance',
-  title: 'An article heading link with an external URL and a URL that should be truncated. but not working',
-};
+  const link4: Item = {
+    type: 'article-heading-link',
+    text: 'https://example.com/this/is/a/long/path',
+    href: 'https://example.com/this/is/a/long/path',
+    title: 'An article heading link with an external URL and a URL that should be truncated.',
+  };
 </script>
 
 <div class="m-auto w-[500px] mt-[50px] flex flex-col gap-4">
-  <Link item={link}>{link.text}</Link>
-  <Link item={link2}>{link2.text}</Link>
-  <Link item={link3}>{link3.text}</Link>
-  <Link item={link4}>{link4.text}</Link>
+  <Link item={link} let:text>{text}</Link>
+  <Link item={link2} let:text>{text}</Link>
+  <Link item={link3} let:text>{text}</Link>
+  <Link item={link4} let:text>{text}</Link>
 </div>
