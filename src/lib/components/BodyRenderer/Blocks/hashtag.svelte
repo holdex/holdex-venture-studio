8ce9d10a45ae8e93412d380258405fb3f52c0078
Addis Belete Bejiga <addis.bel@gmail.com>
2025-05-05 16:28:01 +0300

    chore: optimize hashtag component

--- src/lib/components/BodyRenderer/Blocks/hashtag.svelte
+++ src/lib/components/BodyRenderer/Blocks/hashtag.svelte
@@ -9,22 +9,19 @@
   export let item: Item;
   export let link: string | undefined = undefined;
 
-  let classes = `relative inline-block text-accent1-default transition-colors ${
-    link
-      ? 'bg-accent1-default/15 hover:bg-accent1-default/25 focus:bg-accent1-default/25'
-      : 'bg-transparent hover:bg-transparent'
-  }`;
-
   $: text = item.text || item.href;
 </script>
 
 {' '}
 {#if link}
-  <a title={item.title ? item.title : ''} href={link} class={classes} rel="noreferrer">
+  <a title={item.title ? item.title : ''} href={link} rel="noreferrer">
     <slot {text} />
   </a>
 {:else}
-  <span title={item.title ? item.title : ''} class={classes}>
+  <span
+    title={item.title ? item.title : ''}
+    class="relative inline-block text-accent1-default transition-colors"
+  >
     <slot {text} />
   </span>
 {/if}
