5acedb4714a1f8ad34d2aac950ad616bc8cba31b
williamrusdyputra <williamrusdyputra@gmail.com>
2025-08-06 12:13:44 +0700

    chore: update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['svelte3', '@typescript-eslint', 'prefer-arrow-functions'],
-  // ignorePatterns: ['*.cjs'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-  settings: {
-    'svelte3/typescript': () => require('typescript'),
-    'svelte3/ignore-warnings': (warning) => {
-      return warning.code === 'unused-export-let';
+  plugins: ['svelte', '@typescript-eslint', 'prefer-arrow-functions'],
+  extends: [
+    'eslint:recommended',
+    'plugin:svelte/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.svelte'],
+        sourceType: 'module',
+        ecmaVersion: 2020,
+      },
     },
-  },
+  ],
   globals: {
-    globalThis: false, // means it is not writeable
+    globalThis: false,
   },
   parserOptions: {
     sourceType: 'module',
@@ -26,19 +35,8 @@ module.exports = {
   rules: {
     'comma-dangle': 'off',
     quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-    // 'no-use-before-define': [
-    // 	'error',
-    // 	{
-    // 		functions: true,
-    // 		classes: true,
-    // 		variables: true,
-    // 		allowNamedExports: false,
-    // 	},
-    // ],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
-    // 'import/no-duplicates': ['off', {}], // warnings such as duplicate imports in module and source in the same file are to be ignored,
-    curly: ['error', 'multi-line'],
-  },
-};
+  }
+}
