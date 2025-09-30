263462907d2cf62262e9d2e7719f5676f0fd014d
risyandi <risyandi.94@gmail.com>
2025-09-30 11:59:56 +0700

    refactor(routes): redirect version by params

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-  import { setContext } from 'svelte';
+  import { setContext, onMount } from 'svelte';
   import type { LayoutData } from './$types';
   import { page } from '$app/stores';
-  import { goto } from '$app/navigation';
 
   import '../lib/components/Tracking/scripts';
 
@@ -10,16 +9,14 @@
 
   // detect version from url query parameter
   $: isV2 = $page.url.searchParams.has('v2');
-  console.log('isV2', isV2);
-
   setContext<string>('deploymentUrl', data.deploymentUrl);
-  setContext<boolean>('isV2', isV2);
 
-  // redirect to v2
-  $: if (isV2 && !$page.url.pathname.includes('/v2/')) {
-    const newPath = $page.url.pathname.replace('/v2/', '/');
-    goto(newPath);
-  }
+  onMount(() => {
+    if (isV2 && !$page.url.pathname.includes('/v2/')) {
+      const newPath = $page.url.pathname.replace('/', '/v2/');
+      window.location.href = newPath;
+    }
+  });
 </script>
 
 <slot />
