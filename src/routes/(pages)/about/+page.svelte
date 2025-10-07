08417acf4e260602cb49922806f2aeed4767afbc
risyandi <risyandi.94@gmail.com>
2025-10-07 22:14:00 +0700

    fix(ui): view header image and og about page

--- src/routes/(pages)/about/+page.svelte
+++ src/routes/(pages)/about/+page.svelte
@@ -4,7 +4,9 @@
   import Parser, { type Message } from '$components/BodyParser';
   import BodyRenderer from '$components/BodyRenderer/index.svelte';
   import PageTitle from '$components/PageTitle/index.svelte';
+  import ArticleHeaderImage from '$components/ArticleHeaderImage/index.svelte';
   import { routes } from '$lib/config';
+  import { page } from '$app/stores';
   import type { PageData } from './$types';
 
   export let data: PageData;
@@ -12,13 +14,16 @@
   $: ({ store } = data);
   $: ({ data: storeData } = $store);
   $: message = Parser.parse(storeData?.message as Message);
+
+  // detect version from url query parameter
+  $: isV2 = $page.url.searchParams.get('v2') === 'true';
 </script>
 
 <MetaTags
   title={message.title}
   description={message.subtitle}
   path={routes.about}
-  imagePath="/og/about.png"
+  imagePath="/og/v2/about.png"
 />
 
 <template lang="pug" src="./template.pug">
