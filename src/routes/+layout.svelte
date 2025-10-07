6b2e2f85dfb6ce26cf4a30631267d5cdc1bfcfa4
risyandi <risyandi.94@gmail.com>
2025-10-07 22:11:36 +0700

    chore(layout): remove func not used

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,12 @@
 <script lang="ts">
   import { setContext, onMount } from 'svelte';
   import type { LayoutData } from './$types';
-  import { page } from '$app/stores';
 
   import '../lib/components/Tracking/scripts';
 
   export let data: LayoutData;
 
-  // detect version from url query parameter
-  $: isV2 = $page.url.searchParams.has('v2');
   setContext<string>('deploymentUrl', data.deploymentUrl);
-
-  onMount(() => {
-    if (isV2 && !$page.url.pathname.includes('/v2/')) {
-      const newPath = $page.url.pathname.replace('/', '/v2/');
-      window.location.href = newPath;
-    }
-  });
 </script>
 
 <slot />
