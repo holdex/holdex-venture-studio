9180f14c54e53040b49b0297af18ef654740dd39
risyandi <risyandi.94@gmail.com>
2025-10-07 22:56:18 +0700

    fix(ui): view header image and og forstartup page

--- src/routes/(pages)/for-startups/+page.svelte
+++ src/routes/(pages)/for-startups/+page.svelte
@@ -4,7 +4,9 @@
   import Parser, { type Message } from '$components/BodyParser';
   import BodyRenderer from '$components/BodyRenderer/index.svelte';
   import PageTitle from '$components/PageTitle/index.svelte';
+  import ArticleHeaderImage from '$components/ArticleHeaderImage/index.svelte';
   import { routes } from '$lib/config';
+  import { page } from '$app/stores';
   import type { PageData } from './$types';
 
   export let data: PageData;
@@ -12,6 +14,9 @@
   $: ({ store } = data);
   $: ({ data: storeData } = $store);
   $: message = Parser.parse(storeData?.message as Message);
+
+  // detect version from url query parameter
+  $: isV2 = $page.url.searchParams.get('v2') === 'true';
 </script>
 
 <MetaTags
